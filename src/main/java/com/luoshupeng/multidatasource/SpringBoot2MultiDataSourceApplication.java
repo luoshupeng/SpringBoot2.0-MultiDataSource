5648cc5f2e1276572a2073b036ba95de3b1d5766
luoshupeng <shupengluo@gmail.com>
2018-03-20 08:16:12 +0800

    Init Commit

---
+++ src/main/java/com/luoshupeng/multidatasource/SpringBoot2MultiDataSourceApplication.java
@@ -0,0 +1,12 @@
+package com.luoshupeng.multidatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2MultiDataSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2MultiDataSourceApplication.class, args);
+	}
+}
